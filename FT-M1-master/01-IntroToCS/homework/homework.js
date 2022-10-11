ef8425d189feb8be37a80cd50ee58e6dc5eb2ba6
unknown <fabian37gomez@gmail.com>
2022-10-11 16:58:33 -0500

    Homework CS updated

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,8 +1,29 @@
 'use strict';
+let array = ['10110']
+         
+function BinarioADecimal(num) {
+   // let decimal = 0;
+   //  for (let i = 0; i < num.length; i++) {
+   //     decimal = decimal + num[i] * 2 ** (num.length - 1 - i);
+   //  }
+   //  return decimal;
+   return parseInt(num, 2)
+}
 
-function BinarioADecimal(num) {}
+function DecimalABinario(num) {
+   let binario = [];
+   while (num != 0) {
+      let numRedondeado = Math.floor(num/2)   
+      let residuo = parseInt(num%2)
+      num = numRedondeado
+      binario.push(residuo)
+   }
 
-function DecimalABinario(num) {}
+   binario.reverse()
+   binario.toString()
+   return binario.join('');
+   // return num.toString(2)
+}
 
 module.exports = {
    BinarioADecimal,
