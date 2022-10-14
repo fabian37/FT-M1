b7beda5564a2bd74fc77e0257687bb6378a0f0b8
unknown <fabian37gomez@gmail.com>
2022-10-14 18:28:38 -0500

    homework data structre 1 updated

--- FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
@@ -1,13 +1,53 @@
 'use strict';
 
 // EJERCICIO 1
-function nFactorial(n) {}
+function nFactorial(n) {
+   if ( n<0 ) return 'Es un valor negativo'
+   if ( n===0 || n===1) return 1
+   return n * nFactorial(n-1)
+}
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+   if (n<=1) return n
+   return nFibonacci(n-1) + nFibonacci(n-2)
+}
+// function nFibonacci(n) {
+//    if (n===0) return 0
+//    if (n===1) return 1
+//    return nFibonacci(n-1) + nFibonacci(n-2)
+//  }
 
 // EJERCICIO 3
-function Queue() {}
+// //funcion constructura
+//clases ECMAscript 2015
+class Queue {
+   constructor () {
+      this.array = []
+   }
+   enqueue (elemento) {
+      return this.array.push(elemento)
+   }
+   dequeue () {
+      return this.array.shift()
+   }
+   size () {
+      return this.array.length
+   }
+}
+// function Queue() {
+//    this.array = [];
+// }
+// Queue.prototype.enqueue = function(elemento) {
+//    return this.array.push(elemento);
+// }
+// Queue.prototype.dequeue = function() {
+//    return this.array.shift();
+// }
+// Queue.prototype.size = function() {
+//    return this.array.length;
+// }
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
