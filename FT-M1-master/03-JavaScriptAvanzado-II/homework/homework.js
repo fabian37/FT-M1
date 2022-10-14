70f9f239a87c44d0d3469ae7311dff7647df169b
unknown <fabian37gomez@gmail.com>
2022-10-14 10:05:27 -0500

    homework JSII updated

--- FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
+++ FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
@@ -22,6 +22,7 @@ function counter() {
     return _contador;
   }
 }
+
 // const nuevoContador = counter()
 // nuevoContador()
 // counter()() <-- las dos lineas de arriba son lo mismo que esto
@@ -44,16 +45,25 @@ otra vez cálculos que ya se hicieron anteriormente.
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) */
 
+// function cacheFunction(cb) {
+//   let obj = {}
+//   return function (key) {
+//     if (obj.hasOwnProperty(key)) {
+//       return obj[key]
+//     } else {
+//       let value = cb(key)
+//       obj[key]=value
+//       return obj[key]
+//     }
+//   }
+// }
 function cacheFunction(cb) {
-  let obj = {}
-  return function (key) {
-    if (obj.hasOwnProperty(key)) {
-      return obj[key]
-    } else {
-      let value = cb(key)
-      obj[key]=value
-      return obj[key]
-    }
+  let cache = {}
+  return function (arg) {
+    //IF = si la propiedad no está, lo voy a calcular por primera vez, almacenarlo en la "cache" y retornarlo
+    //ELSE = si el resultado de la operacion ya esta en la memoria en la "cache", lo retorno de ahí directamente
+    if (!cache.hasOwnProperty(arg)) return cache[arg]=cb(arg)      
+    return cache[arg]
   }
 }
 
